8398f87d280e848bf734579ca6fdcee37e07dde2
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2022-06-06 14:20:12 +0100

    small improvement to WatchItem admin

--- movies/admin.py
+++ movies/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from movies.models import Genre, TitleType, WatchItem
 
 # Register your models here.
-admin.site.register(WatchItem)
 admin.site.register(Genre)
 admin.site.register(TitleType)
+
+
+@admin.register(WatchItem)
+class WatchItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "primary_title",
+        "average_rating",
+        "start_year",
+        "num_votes",
+        "imdb_id",
+    )
